d1c9c2e1db4fcd9d993aad5af1f88cc73660b3fc
Marco Albero <marco@mybi.es>
2016-09-27 13:02:09 +0200

    added json validation for tasks/commands

--- tests/test-validators/test_validators.py
+++ tests/test-validators/test_validators.py
@@ -13,7 +13,6 @@ env = helpers.env.read_config()
 
 @pytest.mark.validators
 class TestValidators:
-
     schema = {
         "type": "array",
         "items": {"type": "number", "enum": [1, 2, 3]},
@@ -29,3 +28,154 @@ class TestValidators:
     def test_json_valid(self):
         result = JsonValidator.validate(TestValidators.json_valid, TestValidators.schema)
         assert result['valid'] is True
+
+    schema2 = {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "definitions": {
+            "task": {
+                "type": "object",
+                "properties": {
+                    "id": {
+                        "type": "string",
+                        "pattern": "^[A-Za-z0-9\\.-_]+$"
+                    },
+                    "parallel_tasks": {
+                        "type": "array",
+                        "properties": {
+                            "anyOf": [
+                                {
+                                    "$ref": "#/definitions/task"
+                                },
+                                {
+                                    "$ref": "#/definitions/command"
+                                }
+                            ]
+                        }
+                    },
+                    "sequential_tasks": {
+                        "type": "array",
+                        "properties": {
+                            "anyOf": [
+                                {
+                                    "$ref": "#/definitions/task"
+                                },
+                                {
+                                    "$ref": "#/definitions/command"
+                                }
+                            ]
+                        }
+                    }
+                },
+                "required": [
+                    "id"
+                ],
+                "anyOf": [
+                    {
+                        "required": ["parallel_tasks"]
+                    },
+                    {
+                        "required": ["sequential_tasks"]
+                    }
+                ],
+                "additionalProperties": False
+            },
+            "command": {
+                "type": "object",
+                "properties": {
+                    "id": {
+                        "type": "string",
+                        "pattern": "^[A-Za-z0-9\\.-_]+$"
+                    },
+                    "env": {
+                        "type": "array",
+                        "properties": {
+                            "property": {
+                                "type": "string"
+                            },
+                            "value": {
+                                "type": "string"
+                            }
+                        }
+                    },
+                    "cmd": {
+                        "type": "string"
+                    }
+                },
+                "required": [
+                    "id", "cmd"
+                ],
+                "additionalProperties": False
+            }
+        },
+
+        "type": "object",
+        "properties": {
+            "build": {
+                "type": "string"
+            },
+            "starter": {
+                "anyOf": [
+                    {
+                        "$ref": "#/definitions/task"
+                    },
+                    {
+                        "$ref": "#/definitions/command"
+                    }
+                ]
+            }
+        },
+        "required": ["build", "starter"],
+        "additionalProperties": False
+    }
+
+    json_build = {
+        "build": "my brand new build",
+        "starter": {
+            "id": "My_task_id_125.task",
+            "sequential_tasks": []
+        }
+    }
+
+    json_build2 = {
+        "build": "my brand new build",
+        "starter": {
+            "id": "My_task_id_125.task",
+            "parallel_tasks": []
+        }
+    }
+
+    json_build3 = {
+        "build": "my brand new build",
+        "starter": {
+            "id": "My_task_id_125.task",
+            "parallel_tasks": [],
+            "sequential_tasks": []
+        }
+    }
+
+    json_build4 = {
+        "build": "my brand new build",
+        "starter": {
+            "id": "My_task_id_125.task",
+            "cmd": "ls -l"
+        }
+    }
+
+    json_build5 = {
+        "build": "my brand new build",
+        "starter": {
+            "id": "My_task_id_125.task"
+        }
+    }
+
+    def test_json_build(self):
+        result = JsonValidator.validate(TestValidators.json_build, TestValidators.schema2)
+        assert result['valid'] is True
+        result = JsonValidator.validate(TestValidators.json_build2, TestValidators.schema2)
+        assert result['valid'] is True
+        result = JsonValidator.validate(TestValidators.json_build3, TestValidators.schema2)
+        assert result['valid'] is True
+        result = JsonValidator.validate(TestValidators.json_build4, TestValidators.schema2)
+        assert result['valid'] is True
+        result = JsonValidator.validate(TestValidators.json_build5, TestValidators.schema2)
+        assert result['valid'] is False
